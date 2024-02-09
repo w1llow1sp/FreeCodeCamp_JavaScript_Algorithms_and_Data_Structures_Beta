1531ac23f0f3a5aab1eea858cf85e0b51c71a22c
Mary <mari.front.74@gmail.com>
2024-02-09 13:10:14 +0500

    Step 38,39

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -20,6 +20,8 @@ const monsterName   = document.querySelector('#monsterName')
 const monsterHealthText   = document.querySelector('#monsterHealth')
 
 //initialize buttons
+
+
 function goStore () {
     console.log("Going to store.");
 }
@@ -30,3 +32,5 @@ function goCave  (){
 function fightDragon  () {
     console.log("Fighting dragon.");
 }
+
+
