896689d82f8cea76d4313ad9fcda9c6f190ea1a4
Mary <mari.front.74@gmail.com>
2024-02-13 17:08:25 +0500

    Step 76

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -67,13 +67,13 @@ function fightDragon  () {
 }
 
 function buyHealth () {
-    //gold = gold -10
-    gold -= 10
-    health +=10
+    if( gold >= 10) {
+        gold -= 10
+        health +=10
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    }
 
-    // Update the innerText of goldText and healthText
-    goldText.innerText = gold;
-    healthText.innerText = health;
 }
 function buyWeapon () {}
 function fightSlime   (){}
