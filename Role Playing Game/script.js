30cb985d62b5e06c076224a0e53924aa126c6fc1
Mary <mari.front.74@gmail.com>
2024-02-19 12:31:08 +0500

    Step 116

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -144,6 +144,8 @@ function fightDragon() {
     update(locations[3])
      monsterHealth = monsters[fighting].health;
     monsterStats.style.display ='block'
+     monsterName.innerText =  monsters[fighting].name;
+     monsterHealthText.innerText = monsterHealth;
  }
 
 function attack () {}
