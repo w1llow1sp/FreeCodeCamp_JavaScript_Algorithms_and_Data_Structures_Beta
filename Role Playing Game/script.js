f1d879c55ad8d5f2b9a809d58c24c012d1149d48
Mary <mari.front.74@gmail.com>
2024-02-19 12:17:42 +0500

    Step 111

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -136,6 +136,9 @@ function fightDragon() {
 }
  function goFight() {}
 
+function attack () {}
+function dodge () {}
+
 button1.onclick = goStore
 button2.onclick = goCave
 button3.onclick = fightDragon
