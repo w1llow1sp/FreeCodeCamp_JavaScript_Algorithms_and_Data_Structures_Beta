1f6e67e8951ca33c8ebc98108b8430b28a5cb48a
Mary <mari.front.74@gmail.com>
2024-02-20 13:42:41 +0500

    Step 146

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -172,7 +172,7 @@ function goFight() {
 function getMonsterAttackValue (level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp))
     console.log(hit)
-    return hit
+    return hit >0 ? hit : 0
 }
 
 function attack() {
