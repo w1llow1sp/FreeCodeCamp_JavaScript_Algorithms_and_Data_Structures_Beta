5558f3fbcd9f01775f24cb12b3c336a77af3229b
Mary <mari.front.74@gmail.com>
2024-02-22 15:36:01 +0500

    Step 173

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -264,7 +264,12 @@ function  pick (guess){
         text.innerText += "Wrong! You lose 10 health!"
         health-=10
         healthText.innerText= health
+        if( health <= 0) {
+            lose()
+
+        }
     }
+
 }
 function  pickTwo  (){
     pick(2)
