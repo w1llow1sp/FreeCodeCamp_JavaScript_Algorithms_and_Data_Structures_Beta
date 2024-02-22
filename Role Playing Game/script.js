cd85554a7209644c66b27a52604f3f64e8a4b343
Mary <mari.front.74@gmail.com>
2024-02-22 15:01:57 +0500

    Step 153

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -196,7 +196,9 @@ function attack() {
             defeatMonster();
         }
     }
-    if(Math.random() <= .1) {}
+    if(Math.random() <= .1) {
+        text.innerText += " Your "+inventory.pop() +" breaks."
+    }
 }
 
 function isMonsterHit() {
