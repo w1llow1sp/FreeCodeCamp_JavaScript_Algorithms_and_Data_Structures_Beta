19a9a97e53fc158a7ef88f61ab5e342b96d252f2
Mary <mari.front.74@gmail.com>
2024-02-24 13:55:05 +0500

    Step 46

--- Calorie counter/script.js
+++ Calorie counter/script.js
@@ -7,18 +7,19 @@ const output = document.getElementById('output')
 
 let isError = false
 
-function cleanInputString (str){
-    let regex=/[+-\s]/g
-    return str.replace(regex,'')
+function cleanInputString(str) {
+    let regex = /[+-\s]/g
+    return str.replace(regex, '')
 }
 
-function isInvalidInput (str) {
+function isInvalidInput(str) {
     let regex = /\d+e\d+/i
     return str.match(regex)
 }
 
-function addEntry () {
+function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
-    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length ;
-    const HTMLString = `<label>Entry ${entryNumber} Name</label>`
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+    const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+                         <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">`
 }
