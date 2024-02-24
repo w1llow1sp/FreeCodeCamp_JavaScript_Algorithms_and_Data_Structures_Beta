eef2f746d42a1f1779a35e75c163f33d1a1017a7
Mary <mari.front.74@gmail.com>
2024-02-24 13:59:16 +0500

    Step 48

--- Calorie counter/script.js
+++ Calorie counter/script.js
@@ -21,5 +21,7 @@ function addEntry() {
     const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`)
     const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
     const HTMLString = `<label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-                         <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">`
+                         <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name">
+<label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+<input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`
 }
