a85821ce3386858e14f3a95b712855f64fc1480e
Mary <mari.front.74@gmail.com>
2024-02-27 13:55:27 +0500

    Step 70

--- Calorie counter/script.js
+++ Calorie counter/script.js
@@ -37,6 +37,8 @@ function calculateCalories (e) {
     const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
     const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
     const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+    let breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs)
 }
 
 function getCaloriesFromInputs(list) {
