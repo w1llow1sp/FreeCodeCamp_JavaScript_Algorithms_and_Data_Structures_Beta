cec53e9bcc4e3100cbf5c1c779a10609124e79aa
Mary <mari.front.74@gmail.com>
2024-02-27 13:28:39 +0500

    Step 62

--- Calorie counter/script.js
+++ Calorie counter/script.js
@@ -34,6 +34,11 @@ let calories =0
     for (const item of list) {
         const currVal = cleanInputString(item.value)
         const  invalidInputMatch = isInvalidInput(currVal)
-        if(invalidInputMatch) {}
+        if (invalidInputMatch) {
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
+            isError = true;
+            return null;
+        }
+
     }
 }
