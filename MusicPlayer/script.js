657546ea4a7adf4870f747f645dfdf1b97d1b12c
Mary <mari.front.74@gmail.com>
2024-03-04 14:49:45 +0500

    Step 4

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -1,8 +1,16 @@
-const playlistSongs = document.getElementById( 'playlist-songs')
+const playlistSongs = document.getElementById('playlist-songs')
 const playButton = document.getElementById('play')
-const pauseButton  = document.getElementById('pause')
-const nextButton  = document.getElementById('next')
-const  previousButton = document.getElementById('previous')
+const pauseButton = document.getElementById('pause')
+const nextButton = document.getElementById('next')
+const previousButton = document.getElementById('previous')
 const shuffleButton = document.getElementById('shuffle')
 
-let allSongs = []
+let allSongs = [
+    {
+        id: 0,
+        title: "Scratching The Surface",
+        artist: "Quincy Larson",
+        duration: "4:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/scratching-the-surface.mp3",
+    }
+]
