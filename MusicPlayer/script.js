3dd24228d09e3e2a54c1d52531a9a11a39939e0b
Mary <mari.front.74@gmail.com>
2024-03-04 15:45:43 +0500

    Step 25

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -64,6 +64,8 @@ const sortSongs = () => {
     userData?.songs.sort((a,b) =>{
         if (a.title < b.title) {
             return -1
+        } if (a.title > b.title) {
+            return 1
         }
     })
 }
