7a3880d3359fc60353bb2c2283ebbad0ef63fd56
Mary <mari.front.74@gmail.com>
2024-03-06 13:51:12 +0500

    Step 59

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -138,4 +138,8 @@ const highlightCurrentSong = () => {
     playlistSongElements.forEach((songEl) => {
         songEl.removeAttribute('aria-current')
     })
+    if (songToHighlight) {
+        songToHighlight.setAttribute('aria-current','true')
+    }
+
 }
