2e5e1549603607037837182aab8c8c731c7f55aa
Mary <mari.front.74@gmail.com>
2024-03-06 13:09:38 +0500

    Step 32

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -73,4 +73,10 @@ const sortSongs = () => {
 }
 const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id === id);
+        audio.src = song.src
+        audio.title = song.title
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0
+    }
+
 }
