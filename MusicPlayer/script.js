d75ceff4ce5702ec35b484171787a7dcc4d51124
Mary <mari.front.74@gmail.com>
2024-03-06 13:41:13 +0500

    Step 53

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -124,6 +124,8 @@ const playPreviousSong  = () => {
         return
     } else  {
         const currentSongIndex = getCurrentSongIndex()
+        const previousSong = userData?.songs[currentSongIndex -1]
+        playSong(previousSong.id)
     }
 
 }
