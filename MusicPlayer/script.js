01f23f8e345a82367115562ca52ed03f1b6dc6af
Mary <mari.front.74@gmail.com>
2024-03-06 13:18:52 +0500

    Step 38

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -86,6 +86,8 @@ const playSong = (id) => {
     playButton.addEventListener('click',()=>{
         if( !userData?.currentSong) {
             playSong(userData?.songs[0].id)
+        } else {
+            playSong(userData?.currentSong.id)
         }
     })
 }
