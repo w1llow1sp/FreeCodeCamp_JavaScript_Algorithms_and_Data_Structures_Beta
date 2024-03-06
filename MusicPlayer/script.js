9262de6d7c627e6b00efe7eb330b9cafb50a43ce
Mary <mari.front.74@gmail.com>
2024-03-06 13:25:45 +0500

    Step 42

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -93,6 +93,9 @@ const playSong = (id) => {
         }
     })
 }
+
 const pauseSong = () => {
     userData.songCurrentTime = audio.currentTime;
+    playButton.classList.remove('playing')
+    audio.pause()
 }
