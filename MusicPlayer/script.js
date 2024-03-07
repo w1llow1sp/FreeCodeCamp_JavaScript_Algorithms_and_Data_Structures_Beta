7e90fb39b4e8f19d3d342bf47573976b33da3c0f
Mary <mari.front.74@gmail.com>
2024-03-07 14:29:53 +0500

    Step 73

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -166,4 +166,8 @@ const shuffle = () => {
     userData?.songs.sort(()=>Math.random() - 0.5)
     userData.currentSong = null
     userData.songCurrentTime = 0
+    renderSongs(userData?.songs)
+    pauseSong()
+    setPlayerDisplay()
+    setPlayButtonAccessibleText()
 }
