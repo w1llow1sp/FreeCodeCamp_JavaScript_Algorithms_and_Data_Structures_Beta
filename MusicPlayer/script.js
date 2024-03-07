3cdc2d99b4b1cd1c627bccd7794eeb3d694a95f3
Mary <mari.front.74@gmail.com>
2024-03-07 15:55:58 +0500

    Step 87

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -194,7 +194,10 @@ const deleteSong = (id) => {
         resetButton.ariaLabel = "Reset playlist";
         resetButton.appendChild(resetText)
         playlistSongs.appendChild(resetButton)
-        resetButton.addEventListener('click',()=>{})
+        resetButton.addEventListener('click',()=>{
+            userData.songs= [...allSongs]
+
+        })
     }
 }
 
