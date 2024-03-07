6871739f11a6b98060037745ef0847c085cbb0e4
Mary <mari.front.74@gmail.com>
2024-03-07 14:14:39 +0500

    Step 69

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -95,6 +95,7 @@ const playSong = (id) => {
         highlightCurrentSong()
     })
     setPlayerDisplay()
+    setPlayButtonAccessibleText()
 }
 
 const pauseSong = () => {
@@ -157,5 +158,7 @@ const setPlayerDisplay = () => {
 };
 const setPlayButtonAccessibleText = () => {
     const song = userData?.currentSong || userData?.songs[0]
+    playButton.setAttribute('aria-label', song?.title ? `Play ${song.title}` : "Play");
+
 }
 
