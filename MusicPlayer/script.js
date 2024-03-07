a988665c609f222bca8ec5846de947a51f59bf3b
Mary <mari.front.74@gmail.com>
2024-03-07 15:48:29 +0500

    Step 83

--- MusicPlayer/script.js
+++ MusicPlayer/script.js
@@ -189,5 +189,6 @@ const deleteSong = (id) => {
     }
     if(userData?.songs.length ===0) {
         const resetButton = document.createElement('button')
+        const resetText = document.createTextNode('Reset Playlist')
     }
 }
