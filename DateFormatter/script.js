6d4a6a0b8f532dcc8494ee5f46e423eaabbf6784
Mary <mari.front.74@gmail.com>
2024-03-09 18:03:22 +0500

    Step 8

--- DateFormatter/script.js
+++ DateFormatter/script.js
@@ -4,4 +4,5 @@ const date  = new Date()
 const day = date.getDate()
 const month = date.getMonth() +1
 const year  = date.getFullYear()
-const hours   = date.getHours()
+const hours = date.getHours()
+const minutes = date.getMinutes()
