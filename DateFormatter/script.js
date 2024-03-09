d7ce9809f92e0b00f536babb28e23c3b32eee511
Mary <mari.front.74@gmail.com>
2024-03-09 18:15:44 +0500

    Step 19

--- DateFormatter/script.js
+++ DateFormatter/script.js
@@ -10,7 +10,9 @@ currentDateParagraph.textContent = formattedDate;
 dateOptionsSelectElement.addEventListener('change', ()=>{
     switch (dateOptionsSelectElement.value) {
         case 'yyyy-mm-dd':
-            currentDateParagraph.textContent = formattedDate;
+            currentDateParagraph.textContent = formattedDate
+                .split('-')
+                .reverse()
     }
 })
 
