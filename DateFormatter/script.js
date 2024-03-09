bb0f79150688925029d4431608cb419325d19f64
Mary <mari.front.74@gmail.com>
2024-03-09 17:47:40 +0500

    Step 5

--- DateFormatter/script.js
+++ DateFormatter/script.js
@@ -2,3 +2,4 @@ const currentDateParagraph = document.getElementById('current-date')
 const dateOptionsSelectElement = document.getElementById('date-options')
 const date  = new Date()
 const day = date.getDate()
+const month = date.getMonth() +1
