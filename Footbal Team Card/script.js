872121189216c6952aca143ec0ec7ca2dd14b35b
Mary <mari.front.74@gmail.com>
2024-03-11 12:31:35 +0500

    Step 29

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -43,7 +43,7 @@ headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
         `<div class="player-card">
-<h2>${name}</h2>
+<h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
          </div>`
     })
 }
