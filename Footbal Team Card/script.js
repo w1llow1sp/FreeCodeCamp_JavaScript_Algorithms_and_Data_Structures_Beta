113ef877a72dc40f3dff7e9ec7d7a1ed755a1934
Mary <mari.front.74@gmail.com>
2024-03-11 12:50:16 +0500

    Step 41

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -55,6 +55,8 @@ playersDropdownList.addEventListener('change',(e)=>{
    playerCards.innerHTML=``
     switch (e.target.value) {
         case 'nickname':
+            setPlayerCards(players.filter((player) => player.nickname !== null));
+            break;
     }
 })
 
