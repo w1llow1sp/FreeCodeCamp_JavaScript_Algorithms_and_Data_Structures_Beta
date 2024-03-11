73a1609a5349a5aaa1496492df13a94e3c269466
Mary <mari.front.74@gmail.com>
2024-03-11 12:33:45 +0500

    Step 30

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -44,6 +44,7 @@ const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
         `<div class="player-card">
 <h2>${name}${isCaptain ? "(Captain)" : ""}</h2>
+<p>Position:${position}</p>
          </div>`
     })
 }
