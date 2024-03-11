57e06ff0e14f17cd6f1a50f4cbd592d0ae0e9b47
Mary <mari.front.74@gmail.com>
2024-03-11 12:29:41 +0500

    Step 28

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -3,19 +3,20 @@ const typeOfSport = document.getElementById('sport')
 const worldCupYear = document.getElementById('year')
 const headCoach = document.getElementById('head-coach')
 const playerCards = document.getElementById('player-cards')
-const playersDropdownList  = document.getElementById('players')
+const playersDropdownList = document.getElementById('players')
 
-const myFavoriteFootballTeam  = {
-    team:'Argentina',
-    sport:'Football',
-    year:1986,
-    isWorldCupWinner:true,
+const myFavoriteFootballTeam = {
+    team: 'Argentina',
+    sport: 'Football',
+    year: 1986,
+    isWorldCupWinner: true,
     headCoach: {
-        coachName:'Carlos Bilardo',
-        matches:7
+        coachName: 'Carlos Bilardo',
+        matches: 7
     },
-    players:[
-        {name: "Sergio Almirón",
+    players: [
+        {
+            name: "Sergio Almirón",
             position: "forward",
             number: 1,
             isCaptain: false,
@@ -32,16 +33,18 @@ const myFavoriteFootballTeam  = {
 }
 
 Object.freeze(myFavoriteFootballTeam)
-const {sport, team , year, players , headCoach} = myFavoriteFootballTeam
+const {sport, team, year, players, headCoach} = myFavoriteFootballTeam
 const {coachName} = myFavoriteFootballTeam.headCoach
 typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 
-const setPlayerCards = (arr= players) => {
-    playerCards.innerHTML+= arr.map(({name,position,number,isCaptain, nickname}) => {
-        `<div class="player-card"></div>`
+const setPlayerCards = (arr = players) => {
+    playerCards.innerHTML += arr.map(({name, position, number, isCaptain, nickname}) => {
+        `<div class="player-card">
+<h2>${name}</h2>
+         </div>`
     })
 }
 
