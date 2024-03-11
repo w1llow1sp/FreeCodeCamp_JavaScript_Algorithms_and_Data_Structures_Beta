89ea6c8752764b42f58350db8cfd0cb7ce6a7c25
Mary <mari.front.74@gmail.com>
2024-03-11 11:55:31 +0500

    Step 10

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -9,5 +9,9 @@ const myFavoriteFootballTeam  = {
     team:'Argentina',
     sport:'Football',
     year:1986,
-    isWorldCupWinner:true
+    isWorldCupWinner:true,
+    headCoach: {
+        coachName:'Carlos Bilardo',
+        matches:7
+    }
 }
