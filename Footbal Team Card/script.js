103f65a719fb4cd49c21d07fc55e40749ee66116
Mary <mari.front.74@gmail.com>
2024-03-11 12:54:35 +0500

    Step 47 (end project)

--- Footbal Team Card/script.js
+++ Footbal Team Card/script.js
@@ -69,6 +69,8 @@ playersDropdownList.addEventListener('change',(e)=>{
         case 'goalkeeper':
             setPlayerCards(players.filter((player) => player.position === "goalkeeper"));
             break;
+        default:
+            setPlayerCards();
     }
 })
 
