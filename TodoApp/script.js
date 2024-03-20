4cc924952b2f80507228c62538ff631beab5482d
Mary <mari.front.74@gmail.com>
2024-03-20 19:08:48 +0500

    Step 5

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -12,3 +12,6 @@ const titleInput  = document.getElementById('title-input')
 
 const dateInput   = document.getElementById('date-input')
 const descriptionInput   = document.getElementById('description-input')
+
+const taskData = []
+let currentTask ={}
