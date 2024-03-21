fb2355aa062010b69d600555aaa2c8e46a0752a5
Mary <mari.front.74@gmail.com>
2024-03-21 11:46:14 +0500

    Step 15

--- TodoApp/script.js
+++ TodoApp/script.js
@@ -35,13 +35,17 @@ discardBtn.addEventListener('click', () => {
 
 taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const dataArrIndex = taskData.findIndex((item) => item.id===currentTask.id);
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value
+    }
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj)
+    }
 })
 
-const taskObj = {
-    id:`${titleInput.value.toLowerCase().split(' ').join('-')}-${Date.now()}`,
-    title:titleInput.value,
-    date:dateInput.value,
-    description:descriptionInput.value
-}
+
 
